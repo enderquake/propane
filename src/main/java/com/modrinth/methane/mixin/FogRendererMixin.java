45e152b7f218b78500115cd9c25127102b71cc88
enderquake <oxygen.bert@gmail.com>
2025-09-30 23:51:18 +0200

    idek

--- src/main/java/com/modrinth/methane/mixin/FogRendererMixin.java
+++ src/main/java/com/modrinth/methane/mixin/FogRendererMixin.java
@@ -7,7 +7,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(FogRenderer.class)
@@ -16,14 +17,6 @@ public class FogRendererMixin {
      * @author AnOpenSauceDev
      * @reason what fog??!?!? never heard of it.
      */
-
-//    @Inject(method = "Lnet/minecraft/client/render/fog/FogRenderer;applyFog(Lnet/minecraft/client/render/Camera;IZLnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;", at = @At(value="INVOKE",target="Lnet/minecraft/client/render/fog/FogRenderer;applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V",shift=At.Shift.BEFORE), cancellable = true)
-//    private void applyFog(Camera camera, int viewDistance, boolean thick, RenderTickCounter tickCounter, float skyDarkness, ClientWorld world, CallbackInfoReturnable<Vector4f> cir) {
-//        if (!Methane.ModActive && !Methane.settings.fogSettings.persistFogSettings) return;
-//        if (hasBlindOrDark(camera.getFocusedEntity())) return;
-//        fogData.renderDistanceStart=0;
-//        fogData.renderDistanceEnd = 0;
-//    }
 
     @ModifyArgs(method = "Lnet/minecraft/client/render/fog/FogRenderer;applyFog(Lnet/minecraft/client/render/Camera;IZLnet/minecraft/client/render/RenderTickCounter;FLnet/minecraft/client/world/ClientWorld;)Lorg/joml/Vector4f;",at=@At(value="INVOKE",target="Lnet/minecraft/client/render/fog/FogRenderer;applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V"))
     private void fogDelete(Args args) {
--- src/main/java/com/modrinth/methane/mixin/getLightLevelHack/MushroomPlantMixin.java
+++ src/main/java/com/modrinth/methane/mixin/getLightLevelHack/MushroomPlantMixin.java
@@ -1,15 +1,12 @@
 package com.modrinth.methane.mixin.getLightLevelHack;
 
 import com.modrinth.methane.Methane;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.MushroomPlantBlock;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MushroomPlantBlock.class)
 public class MushroomPlantMixin {
